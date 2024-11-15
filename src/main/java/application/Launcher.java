edc7e400be54c7cd02b81199cd83e03b569158f2
PedroV <a_pedro.vigara.haro@iespablopiccaso.es>
2024-11-15 09:55:48 +0100

    Estructura base

---
+++ src/main/java/application/Launcher.java
@@ -0,0 +1,11 @@
+package application;
+
+public class Launcher {
+
+   public static void main(String[] args) {
+      
+      mainApp.main(args);
+
+   }
+
+}
---
+++ src/main/java/application/mainApp.java
@@ -0,0 +1,35 @@
+
+package application;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class mainApp extends Application {
+
+   @Override
+   public void start(Stage primaryStage) {
+      try {
+         FXMLLoader loader = new FXMLLoader();
+         
+         loader.setLocation(mainApp.class.getResource("/views/Login.fxml"));
+         
+         Pane ventana = (Pane) loader.load();
+         
+         Scene scene = new Scene(ventana);
+         
+         primaryStage.setScene(scene);
+         primaryStage.show();
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+   }
+
+   public static void main(String[] args) {
+      launch(args);
+   }
+}
